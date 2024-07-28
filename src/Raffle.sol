2308e07c727993e1dc6b0e1dbea790079f88656c
Muhammad Abdullah <abdullah.se4th@gmail.com>
2024-07-28 11:04:24 +0500

    Custom Errors

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -9,13 +9,21 @@ pragma solidity ^0.8.18;
  */
 
 contract Raffle {
+    //Errors:
+    error Raffle__SendMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        //require(msg.value >= i_entranceFee, "SendMoreToEnterRaffle");
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+    }
 
     function pickWinner() public {}
 
