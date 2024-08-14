0ef150ce2ef6ea0bfe78b489b8e809a56e796ab8
Muhammad Abdullah <abdullah.se4th@gmail.com>
2024-08-14 22:12:13 +0500

    Deployment Script

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -7,7 +7,9 @@ import {HelperConfig} from "script/HelperConfig.s.sol";
 import {CreateSubscription, FundSubscription, AddConsumer} from "script/Intreactions.s.sol";
 
 contract DeployRaffle is Script {
-    function run() public {}
+    function run() public {
+        deployContract();
+    }
 
     function deployContract() public returns (Raffle, HelperConfig) {
         HelperConfig helperConfig = new HelperConfig();
