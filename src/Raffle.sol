3226e7cee81e1c89758b3d51c78f90170410659b
Muhammad Abdullah <abdullah.se4th@gmail.com>
2024-08-15 02:09:00 +0500

    added another getter function

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -174,4 +174,8 @@ contract Raffle is VRFConsumerBaseV2Plus {
     function getLastTimeStamp() external view returns (uint256) {
         return s_lastTimeStamp;
     }
+
+    function getRecentWinner() external view returns (address) {
+        return s_recentWinner;
+    }
 }
